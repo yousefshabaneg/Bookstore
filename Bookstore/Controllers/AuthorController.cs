1dbdd3c5490ae51b110a683deaf9092cc91d3808
Yousef Shaban <yousefshaban.eg@gmail.com>
2021-07-23 15:29:56 +0200

    Dependency Injection.

---
+++ Bookstore/Controllers/AuthorController.cs
@@ -0,0 +1,92 @@
+﻿using Bookstore.Models;
+using Bookstore.Models.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly IBookstoreRepository<Author> authorRepository;
+
+        public AuthorController(IBookstoreRepository<Author> authorRepository)
+        {
+            this.authorRepository = authorRepository;
+        }
+
+        // GET: AuthorController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AuthorController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AuthorController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AuthorController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AuthorController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AuthorController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AuthorController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AuthorController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
