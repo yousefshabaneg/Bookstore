afb7823f488b5df219cdf4a944b7c8b056dae096
Yousef Shaban <yousefshaban.eg@gmail.com>
2021-07-23 15:21:02 +0200

    Authors Repo.

---
+++ Bookstore/Models/Repositories/AuthorRepository.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Bookstore.Models.Repositories
+{
+    public class AuthorRepository : IBookstoreRepository<Author>
+    {
+        List<Author> authors;
+
+        public AuthorRepository()
+        {
+            authors = new List<Author>
+            {
+                new Author{Id=1, FullName="Robert Martin"},
+                new Author{Id=2, FullName="Reda Elbasiouny"},
+                new Author{Id=3, FullName="Mosh Hamedani"},
+            };
+        }
+
+        public void Add(Author author)
+        {
+            authors.Add(author);
+        }
+
+        public void Delete(int id)
+        {
+            authors.Remove(Find(id));
+        }
+
+        public Author Find(int id)
+        {
+            var author = authors.SingleOrDefault(b => b.Id == id);
+            return author;
+        }
+
+        public IList<Author> List()
+        {
+            return authors;
+        }
+
+        public void Update(int id, Author newAuthor)
+        {
+            var author = Find(id);
+
+            author.FullName = newAuthor.FullName;
+        }
+    }
+}
