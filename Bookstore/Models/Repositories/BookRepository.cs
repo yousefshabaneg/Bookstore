3874714cb2233bfccdc71ee5d5f24fbb8437af39
Yousef Shaban <yousefshaban.eg@gmail.com>
2021-07-23 14:40:39 +0200

    Make the Repositories Interface and Book Repository.

---
+++ Bookstore/Models/Repositories/BookRepository.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Bookstore.Models.Repositories
+{
+    public class BookRepository : IBookstoreRepository<Book>
+    {
+        List<Book> books;
+
+        public BookRepository()
+        {
+            books = new List<Book>
+            {
+                new Book{Id=1, Title="ASP.NET CORE", Description="Core 3.1"},
+                new Book{Id=2, Title="Angular Framework", Description="Version 8"},
+                new Book{Id=3, Title="Bootstrap Development", Description="Version 4.5.0"}
+            };
+        }
+
+        public void Add(Book book)
+        {
+            books.Add(book);
+        }
+
+        public void Delete(int id)
+        {
+            books.Remove(Find(id));
+        }
+
+        public Book Find(int id)
+        {
+            var book = books.SingleOrDefault(b => b.Id == id);
+            return book;
+        }
+
+        public IList<Book> List()
+        {
+            return books;
+        }
+
+        public void Update(int id, Book newBook)
+        {
+            var book = Find(id);
+
+            book.Title = newBook.Title;
+            book.Description = newBook.Description;
+            book.Author = newBook.Author;
+        }
+    }
+}
---
+++ Bookstore/Models/Repositories/IBookstoreRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Bookstore.Models.Repositories
+{
+    public interface IBookstoreRepository<TEntity>
+    {
+        IList<TEntity> List();
+
+        TEntity Find(int id);
+
+        void Add(TEntity entity);
+
+        void Update(int id, TEntity entity);
+
+        void Delete(int id);
+    }
+}
